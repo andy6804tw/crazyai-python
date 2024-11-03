7fd03809400faa8476d299bd94be3b024d9772ce
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2024-11-03 21:47:33 +0800

    Deployed bd5fbfb with MkDocs version: 1.6.0

---
+++ javascripts/analytics.js
@@ -0,0 +1,10 @@
+var script = document.createElement('script');
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-6JLR6GBYQT';
+script.async = true;
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', 'G-6JLR6GBYQT');
